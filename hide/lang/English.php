6ba218a884b83cf9471d012378ed55b742090bd0
kanektikys <kanektikys@56b793a0-39b3-11de-9d05-31133e6853b1>
2009-11-03 12:09:27 +0000

    Расширение Hide

---
+++ hide/lang/English.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * manifest file for hide
+ *
+ * @copyright Copyright (C) 2009 KANekT @ http://blog.teamrip.ru
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package hide
+*/
+
+$lang_hide = array(
+	'Hidden text guest'			=>	'Before you can view this text, you have to %s.',
+	'login'                     =>  'login',
+	'Hidden text'				=>	'Hidden text',
+	'Hidden count text begin'	=>	'You need',
+	'Hidden count text end'		=>	'messages or more to view hidden text.',
+);
+?>
