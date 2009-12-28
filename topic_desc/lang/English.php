583b479532b55aac6fa2f9234d806fc2345c2056
kanektikys <kanektikys@56b793a0-39b3-11de-9d05-31133e6853b1>
2009-12-28 12:20:40 +0000

    Расширение - Описание темы

---
+++ topic_desc/lang/English.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Lang file for topic description
+ *
+ * @copyright Copyright (C) 2009 KANekT @ http://blog.teamrip.ru
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package topic description
+*/
+
+$lang_topic = array(
+	'Topic Description'		=> 'Description',
+);
+?>
