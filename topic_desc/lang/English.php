947fe68087295057e40433f7e056c47e6c5e38e6
kanektikys <kanektikys@56b793a0-39b3-11de-9d05-31133e6853b1>
2010-01-25 08:47:36 +0000

    Topic description 0.5.0

--- topic_desc/lang/English.php
+++ topic_desc/lang/English.php
@@ -10,7 +10,8 @@ $lang_topic = array(
 	'Topic Description'		=> 'Description',
 	'Topic features head' => 'Configure Topic Description',
 	'Topic legend' => 'Configure Topic Description',
-	'Topic enable' => 'Show the description when a user mouseover the topic link',
-	'Topic enable legend' => 'Enable',
+	'Topic enable' => 'Show the description after topic',
+	'Topic enable mouse'	=>	'Show the description when a user mouseover the topic link',
+	'Topic enable and mouse'=>	'Show the description after topic and when a user mouseover the topic link',
 );
 ?>
