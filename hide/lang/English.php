43c4f7c1f637abbd449b3b300fd752166e495199
kanektikys@gmail.com <kanektikys@gmail.com@56b793a0-39b3-11de-9d05-31133e6853b1>
2010-05-13 17:52:38 +0000

    markItUp! 0.5.7

--- hide/lang/English.php
+++ hide/lang/English.php
@@ -10,8 +10,8 @@ $lang_hide = array(
 	'Hidden text guest'			=>	'Before you can view this text, you have to %s.',
 	'login'                     =>  'login',
 	'Hidden text'				=>	'Hidden text',
-	'Hidden count text begin'	=>	'You need',
-	'Hidden count text end'		=>	'messages or more to view hidden text.',
+	'Hidden count begin'	=>	'You need',
+	'Hidden count end'		=>	'messages or more to view hidden text.',
 	'Hidden text group'			=>  'Hidden text for group ',
 );
 ?>
