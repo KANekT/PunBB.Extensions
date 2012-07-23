b99604c27a3719740f90c13fef71f6f253286c9c
KANekT <kanektikys@gmail.com>
2012-07-23 18:24:42 +0400

    Hide 0.4.1

---
+++ nya_hide/lang/Russian.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * manifest file for hide
+ *
+ * @copyright Copyright (C) 2009 KANekT @ http://blog.teamrip.ru
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package hide
+*/
+
+$lang_hide = array(
+	'Hidden text guest'	=> 'Вы должны зайти под %s, чтобы увидеть скрытый текст.',
+	'login'				=> 'своим именем',
+	'Hidden text'		=> 'Скрытый текст ',
+	'Hidden count begin'=> 'Количество Ваших сообщений должно быть равным',
+	'Hidden count end'	=> 'или более, чтобы увидеть скрытый текст.',
+	'Hidden text group'	=> 'Скрытый текст для группы ',
+);
