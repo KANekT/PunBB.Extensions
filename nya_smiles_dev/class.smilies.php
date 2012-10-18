c8f6d776c64a2d1ee5150d42483c3ab87bf04bdc
KANekT <kanektikys@gmail.com>
2012-10-18 18:51:26 +0400

    Smiles 0.4.0
    Delete Repeat smiles

--- nya_smiles_dev/class.smilies.php
+++ nya_smiles_dev/class.smilies.php
@@ -17,25 +17,26 @@ class iSmiles {
 		$this->Smiles = array();
 	}
 
-    public function add_Smile($smile) {
-        if (is_null($smile))// || !is_array($Smile)
-            return false;
-        $this->Smiles = array_merge($this->Smiles, $smile);
-    }
+	public function add_Smile($smile) {
+		if (is_null($smile))// || !is_array($Smile)
+			return false;
+		$this->Smiles = array_merge($this->Smiles, $smile);
+	}
 
-    public function add_Smilie($smile, $src, $title = NULL, $width = NULL, $height = NULL) {
-        if (is_null($smile))// || !is_array($Smile)
-            return false;
+    public function add_Smilie($smile, $src, $title = NULL, $width = NULL, $height = NULL, $view = NULL) {
+		if (is_null($smile))// || !is_array($Smile)
+			return false;
 
-            if($title != NULL)
-                $this->Smiles[$smile]['title'] = $title;
-            else
-                $this->Smiles[$smile]['title'] = $smile;
+			if($title != NULL)
+				$this->Smiles[$smile]['title'] = $title;
+			else
+				$this->Smiles[$smile]['title'] = $smile;
 
-            $this->Smiles[$smile]['src'] = $src;
-            $this->Smiles[$smile]['width'] = $width;
-            $this->Smiles[$smile]['height'] = $height;
-    }
+			$this->Smiles[$smile]['src'] = $src;
+			$this->Smiles[$smile]['width'] = $width;
+			$this->Smiles[$smile]['height'] = $height;
+			$this->Smiles[$smile]['view'] = $view;
+	}
 }
 
 $ext_Smiles = $ext_Smilie = new iSmiles();
