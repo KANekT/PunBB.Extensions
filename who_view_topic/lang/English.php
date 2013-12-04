7fde49d60b5387acac7772b015253bb86dea7acd
KANekT <kanektikys@gmail.com>
2013-12-04 17:00:58 +0400

    Who view the topic
    exp Who view the topic

---
+++ who_view_topic/lang/English.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_index = array(
+'Guests plural'				=>	'<strong>%s</strong> guests',
+'Guests single'				=>	'<strong>1</strong> guest',
+'Guests none'				=>	'<strong>0</strong> guests',
+'Users plural'				=>	'<strong>%s</strong> registered users',
+'Users single'				=>	'<strong>1</strong> registered user',
+'Users none'				=>	'<strong>0</strong> registered users',
+'Currently online'			=>	'Currently view post: %s',
+'Online stats separator'	=>	', ',
+'Online list separator'		=>	', ',
+);
---
+++ who_view_topic/lang/Russian.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('FORUM')) die();
+$lang_index = array(
+'Guests plural'				=> '<strong>%s</strong> гостей',
+'Guests single'				=> '<strong>1</strong> гость',
+'Guests none'				=> '<strong>0</strong> гостей',
+'Users plural'				=> '<strong>%s</strong> пользователей',
+'Users single'				=> '<strong>1</strong> пользователь',
+'Users none'				=> '<strong>0</strong> пользователей',
+'Currently online'			=> 'Просматривают тему: %s',
+'Online stats separator'	=> ', ',
+'Online list separator'		=> ', ',
+);
